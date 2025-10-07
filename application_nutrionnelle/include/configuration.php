001aaca723f02afaac8bee8f8216e90920d0a44e
FPatato <louca.cc26@gmail.com>
2025-10-07 10:39:26 -0400

    truc

--- application_nutrionnelle/include/configuration.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-if (!defined('HOST')) define('HOST', 'localhost');
-if (!defined('DONNEE')) define('DONNEE', 'base_de_donnee');
-if (!defined('USER')) define('USER', 'root');
-if (!defined('PASSWORD')) define('PASSWORD', '');
-?>
